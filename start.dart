4c5b4058cefa4b20ced926c6363e95e77b48ff11
Arjun <arjundoel@gmail.com>
2021-12-22 22:53:45 +0000

    testing

--- start.dart
+++ start.dart
@@ -1,1 +1,20 @@
 // Start
+
+void main() {
+  // Data types
+  int num1 = 2;
+  double num2 = 3.0;
+  bool isTrue = true;
+
+  print((num1 + num2) is int);
+  print((num1 + num2).runtimeType);
+
+  String str = 'hello';
+  print('string concat $str, here is a cool expression ${num1 + num2}');
+
+  var username;
+  username = 'hello world';
+
+  final String fullname = 'Jeffrey';
+  // fullname = 'Jefferson'; throws an error
+}
